71208d56f6ec71c71d13f0373406076ef3f4172f
abhishek kumar <abhishekk3.cashify.in>
2026-02-08 20:16:11 +0530

    completed first lecture

---
+++ Assets/Mvp/FinalCharacterController/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace FinalCharacterController
+{
+    [DefaultExecutionOrder(-1)]
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Camera _playerCamera;
+        
+        [Header("Base Movement")]
+        public float runAcceleration = 0.25f;
+        private float runSpeed = 4f;
+        public float drag = 0.1f;
+
+        [Header("Camera Settings")]
+        public float lookSenseH = 0.1f;
+        public float lookSenseV = 0.1f;
+        public float lookLimitV = 89f;
+        
+        private PlayerLocomotionInput _playerLocomotionInput;
+        private Vector2 _cameraRotation = Vector2.zero;
+        private Vector2 _playerTargetRotation = Vector2.zero;
+
+        private void Awake()
+        {
+            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        }
+
+        private void Update()
+        {
+            Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0, _playerCamera.transform.forward.z).normalized;
+            Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0, _playerCamera.transform.right.z).normalized;
+            Vector3 movementDirection = cameraForwardXZ * _playerLocomotionInput.MovementInput.y + cameraRightXZ * _playerLocomotionInput.MovementInput.x;
+            
+            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
+            Vector3 newVelocity = _characterController.velocity + movementDelta;
+            
+            // Add drag to player
+            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+            newVelocity = newVelocity.magnitude > drag * Time.deltaTime ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed );
+            
+            // Move character once per frame
+            _characterController.Move(newVelocity * Time.deltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
+            _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV * _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+            
+            _playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;
+            transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
+            
+            _playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
+        }
+    }
+}
---
+++ Assets/Mvp/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FinalCharacterController
+{
+    [DefaultExecutionOrder(-2)]
+    public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+    {
+        public PlayerControls PlayerControls { get; private set; }
+        public Vector2 MovementInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+        
+        private void Awake()
+        {
+            PlayerControls = new PlayerControls();
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+            print(MovementInput);
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+           LookInput = context.ReadValue<Vector2>();
+        }
+    }
+}
